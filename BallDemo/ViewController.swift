aa4035479a9d3ba620344806e0f6e1ea6f72ea4a
Luong Tiem <luongvantiem@gmail.com>
2016-07-27 17:40:08 +0700

    v1

--- BallDemo/ViewController.swift
+++ BallDemo/ViewController.swift
@@ -9,17 +9,79 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var timer = NSTimer()
+    var radians = CGFloat()
+    var ballRadian = CGFloat()
+    var ball = UIImageView()
+    var checkBien = true
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.015, target: self, selector: #selector(ViewController.rollBall), userInfo: nil, repeats: true)
+        checkBien = true
+        addBall()
+        rollBall()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+ 
+    
 
+    
+    func addBall(){
+        let viewSize = self.view.bounds.size
+        ball = UIImageView(image: UIImage(named: "ball.png"))
+        ballRadian = 32
+        ball.center = CGPointMake(ballRadian , viewSize.height/2)
+        self.view.addSubview(ball)
+        
+    }
+    
+    func rollBall(){
+        
+        let maxX = self.view.bounds.size.width
+        print(ball.center.x)
+        print(maxX)
+        
+        
+        if(checkBien){
+            
+            leftToRight()
+            
+        }else {
+            
+            rightToLeft()
+            
+        }
+    }
+    
+
+    func leftToRight(){
+        
+        let detal: CGFloat = 0.1
+        radians = radians + detal
+        ball.transform = CGAffineTransformMakeRotation(radians)
+        ball.center = CGPointMake(ball.center.x   + radians * detal, ball.center.y)
+        
+        if (ball.center.x > self.view.bounds.size.width - ballRadian) {
+            checkBien = !checkBien
+            radians = -0.2
+        }
+    }
+    
+    func rightToLeft(){
+        let detal: CGFloat = -0.1
+        radians = radians + detal
+        ball.transform = CGAffineTransformMakeRotation(radians)
+        ball.center = CGPointMake(ball.center.x   - radians * detal, ball.center.y)
+        
+        if(ball.center.x < ballRadian){
+            checkBien = true
+            radians = 0.2
+        }
+    }
+   
+
+    
 
 }
 
